bc762e3cd80dd081a939f54b4c9cc481d41176d0
Vucuongtuan <vucuongtuan00@gmail.com>
2023-12-24 21:27:02 +0700

    fix getAllProduct

--- src/service/apiAllProduct/index.js
+++ src/service/apiAllProduct/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const getAllProduct = require("../../controller/AllProductController");
 const routerAllProduct = express.Router();
 
 routerAllProduct.get("/all-product", getAllProduct);

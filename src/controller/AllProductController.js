828cd3eb7f08123ca70d4cebe874f3fe376bba03
Vucuongtuan <vucuongtuan00@gmail.com>
2024-01-10 21:15:44 +0700

    new

--- src/controller/AllProductController.js
+++ src/controller/AllProductController.js
@@ -5,22 +5,18 @@ const PAGE_SIZE = 10;
 const getAllProduct = async (req, res, next) => {
   try {
     const page = parseInt(req.query.page) || 1;
-    const productId = req.query.productId; // ID cụ thể cần tìm kiếm
+    const nameProduct = req.query.nameProduct;
 
-    // Lấy dữ liệu từ ba bảng
     const getDataLaptop = await ProductLaptop.find({});
     const getDataMouse = await Mouse.find({});
     const getDataKeybourd = await Keybourd.find({});
 
-    // Kết hợp kết quả từ ba bảng vào một mảng
     const allData = [...getDataLaptop, ...getDataMouse, ...getDataKeybourd];
 
-    // Thêm query theo ID nếu có
-    const filteredData = productId
-      ? allData.filter((item) => item._id === productId)
+    const filteredData = nameProduct
+      ? allData.filter((item) => item._id === nameProduct)
       : allData;
 
-    // Sử dụng skip và limit trên mảng đã kết hợp
     const startIndex = (page - 1) * PAGE_SIZE;
     const endIndex = startIndex + PAGE_SIZE;
     const newData = filteredData.slice(startIndex, endIndex);

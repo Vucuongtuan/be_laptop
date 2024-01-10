347e20ad78830e0f21cf097691daa565eb72c18d
Vucuongtuan <vucuongtuan00@gmail.com>
2024-01-10 20:24:00 +0700

    a

--- src/controller/AllProductController.js
+++ src/controller/AllProductController.js
@@ -1,30 +1,33 @@
-const { ProductLaptop, Mouse,Keybourd } = require("../models/");
+const { ProductLaptop, Mouse, Keybourd } = require("../models/");
 
-const PAGE_SIZE = 10;
+const PAGE_SIZE = 1;
+
 const getAllProduct = async (req, res, next) => {
   try {
     const page = parseInt(req.query.page) || 1;
 
-    const getDataLaptop = await ProductLaptop.find({})
-      .skip((page - 1) * PAGE_SIZE)
-      .limit(PAGE_SIZE);
-    const getDataMouse = await Mouse.find({})
-      .skip((page - 1) * PAGE_SIZE)
-      .limit(PAGE_SIZE);
-    const getDataKeybourd = await Keybourd.find({})
-    .skip((page - 1 ) * PAGE_SIZE)
-    .limit(PAGE_SIZE)
-    const newData = [...getDataLaptop, ...getDataMouse, ...getDataKeybourd];
-    if (getDataLaptop.length === 0) {
+    const getDataLaptop = await ProductLaptop.find({});
+    const getDataMouse = await Mouse.find({});
+    const getDataKeybourd = await Keybourd.find({});
+
+    const allData = [...getDataLaptop, ...getDataMouse, ...getDataKeybourd];
+
+    const startIndex = (page - 1) * PAGE_SIZE;
+    const endIndex = startIndex + PAGE_SIZE;
+    const newData = allData.slice(startIndex, endIndex);
+
+    if (newData.length === 0) {
       return res.json({
         message: "Không có dữ liệu",
       });
     }
+
     const responseData = {
-      total: newData.length,
-      totalPages: Math.ceil(newData.length / PAGE_SIZE),
+      total: allData.length,
+      totalPages: Math.ceil(allData.length / PAGE_SIZE),
       data: newData,
     };
+
     return res.json(responseData);
   } catch (err) {
     console.log(err);

dbd78cd648e54b3fe1529af5ab9d482a04af796a
Vucuongtuan <vucuongtuan00@gmail.com>
2024-01-10 21:05:33 +0700

    new

--- src/controller/AllProductController.js
+++ src/controller/AllProductController.js
@@ -5,16 +5,25 @@ const PAGE_SIZE = 10;
 const getAllProduct = async (req, res, next) => {
   try {
     const page = parseInt(req.query.page) || 1;
+    const productId = req.query.productId; // ID cụ thể cần tìm kiếm
 
+    // Lấy dữ liệu từ ba bảng
     const getDataLaptop = await ProductLaptop.find({});
     const getDataMouse = await Mouse.find({});
     const getDataKeybourd = await Keybourd.find({});
 
+    // Kết hợp kết quả từ ba bảng vào một mảng
     const allData = [...getDataLaptop, ...getDataMouse, ...getDataKeybourd];
 
+    // Thêm query theo ID nếu có
+    const filteredData = productId
+      ? allData.filter((item) => item._id === productId)
+      : allData;
+
+    // Sử dụng skip và limit trên mảng đã kết hợp
     const startIndex = (page - 1) * PAGE_SIZE;
     const endIndex = startIndex + PAGE_SIZE;
-    const newData = allData.slice(startIndex, endIndex);
+    const newData = filteredData.slice(startIndex, endIndex);
 
     if (newData.length === 0) {
       return res.json({
@@ -23,8 +32,8 @@ const getAllProduct = async (req, res, next) => {
     }
 
     const responseData = {
-      total: allData.length,
-      totalPages: Math.ceil(allData.length / PAGE_SIZE),
+      total: filteredData.length,
+      totalPages: Math.ceil(filteredData.length / PAGE_SIZE),
       data: newData,
     };
 

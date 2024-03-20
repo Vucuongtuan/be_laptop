48fc6912481b1362159a8f57935030b2596a75e6
VuTuanCuong <114804308+Vucuongtuan@users.noreply.github.com>
2024-03-20 18:08:10 +0700

    Update AccountController.js

--- src/controller/AccountController.js
+++ src/controller/AccountController.js
@@ -77,8 +77,8 @@ const sendOTP = async (email, otp) => {
   try {
     const transporter = nodemailer.createTransport({
       service: "gmail",
-      host: process.env.MAIL_SMTP,
-      port: process.env.MAIL_PORT,
+      host: "smtp.gmail.com",
+      port: 587,
       secure: false,
       auth: {
         user: process.env.AUTH_EMAIL_SEND_OTP,

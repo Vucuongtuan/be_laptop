ff80cc07bd9b6760d53b916ec85811dc2b5ece8a
VuTuanCuong <114804308+Vucuongtuan@users.noreply.github.com>
2024-03-22 13:12:13 +0700

    Update index.js

--- src/middleware/checkOTP/index.js
+++ src/middleware/checkOTP/index.js
@@ -1,11 +1,21 @@
-const verifyOTP = (req, res, next) => {
+const { OTP } = require("../../models");
+
+const verifyOTP = async (req, res, next) => {
   const { otp, email } = req.body;
-  if (req.session.otp === otp && req.session.email === email) {
-    next();
-  } else {
-    return res.status(400).json({
-      message: "Mã OTP không hợp lệ.",
-    });
+
+  try {
+    const otpRecord = await OTP.findOne({ email, otp }).exec();
+
+    if (otpRecord) {
+      res.status(200).json({ message: "OTP verification successful" });
+      next();
+    } else {
+      res.status(400).json({ message: "Mã OTP không hợp lệ." });
+    }
+  } catch (err) {
+    console.error("Error verifying OTP:", err);
+    res.status(500).json({ message: "Error verifying OTP" });
   }
 };
+
 module.exports = verifyOTP;
